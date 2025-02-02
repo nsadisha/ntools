667ab8b9077e80558f54c4358c0fdfd9f5dd152e
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-02 18:01:17 +0530

    Tool not found component initiated

---
+++ src/app/tools/tool-not-found/tool-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './tool-not-found.component.html',
+  styleUrl: './tool-not-found.component.scss'
+})
+export class ToolNotFoundComponent {
+
+}
