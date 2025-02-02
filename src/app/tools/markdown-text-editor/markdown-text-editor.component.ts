53ba70399cb0bd272c59fab4bfc54687a69f2909
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-02 18:00:49 +0530

    Markdown text editor initiated

---
+++ src/app/tools/markdown-text-editor/markdown-text-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-text-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './markdown-text-editor.component.html',
+  styleUrl: './markdown-text-editor.component.scss'
+})
+export class MarkdownTextEditorComponent {
+
+}
