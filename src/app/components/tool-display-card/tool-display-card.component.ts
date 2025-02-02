c0bf7dc28a04e097c4623db71058aa354f41976e
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-02 22:24:39 +0530

    Tool Display Card

---
+++ src/app/components/tool-display-card/tool-display-card.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {Tool} from "../../model/tool.model";
+import {RouterLink} from "@angular/router";
+import {NzTypographyComponent} from "ng-zorro-antd/typography";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+
+@Component({
+  selector: 'app-tool-display-card',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NzTypographyComponent,
+    NzIconDirective,
+  ],
+  templateUrl: './tool-display-card.component.html',
+  styleUrl: './tool-display-card.component.scss'
+})
+export class ToolDisplayCardComponent {
+  @Input({required: true}) tool!: Tool;
+}
