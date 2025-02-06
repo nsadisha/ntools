3b81ff218dc7f690cd325f28c27b72d80a6f457b
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-06 17:07:32 +0530

    markdown related configurations

---
+++ src/app/config/markdown/mermaid.config.ts
@@ -0,0 +1,7 @@
+import {MermaidAPI} from "ngx-markdown";
+
+export const mermaidOptions: MermaidAPI.Config = {
+  fontFamily: '"trebuchet ms", verdana, arial, sans-serif',
+  logLevel: undefined,
+  theme: MermaidAPI.Theme.Default,
+};
