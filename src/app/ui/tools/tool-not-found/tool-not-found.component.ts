6d60d6f4617b3497d8b88ed64f466d4e7aaaf8bb
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-06 22:36:23 +0530

    tool not found page completed

--- src/app/ui/tools/tool-not-found/tool-not-found.component.ts
+++ src/app/ui/tools/tool-not-found/tool-not-found.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzResultComponent} from "ng-zorro-antd/result";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tool-not-found',
   standalone: true,
-  imports: [],
+    imports: [
+        NzButtonComponent,
+        NzResultComponent,
+        RouterLink
+    ],
   templateUrl: './tool-not-found.component.html',
   styleUrl: './tool-not-found.component.scss'
 })
