b7bc46a4771c166086624a16a21983ef346060b8
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-06 21:47:36 +0530

    Add tooltip for tool display card

--- src/app/ui/components/tool-display-card/tool-display-card.component.ts
+++ src/app/ui/components/tool-display-card/tool-display-card.component.ts
@@ -3,15 +3,17 @@ import {Tool} from "../../../model/tool.model";
 import {RouterLink} from "@angular/router";
 import {NzTypographyComponent} from "ng-zorro-antd/typography";
 import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzTooltipDirective} from "ng-zorro-antd/tooltip";
 
 @Component({
   selector: 'app-tool-display-card',
   standalone: true,
-  imports: [
-    RouterLink,
-    NzTypographyComponent,
-    NzIconDirective,
-  ],
+    imports: [
+        RouterLink,
+        NzTypographyComponent,
+        NzIconDirective,
+        NzTooltipDirective,
+    ],
   templateUrl: './tool-display-card.component.html',
   styleUrl: './tool-display-card.component.scss'
 })
