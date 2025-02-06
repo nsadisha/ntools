e46f810960ccf922252130776582844400c7d801
Sadisha Nimsara <nsadisha@gmail.com>
2025-02-06 14:20:25 +0530

    Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzTypographyComponent} from "ng-zorro-antd/typography";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NzIconDirective
+    NzIconModule,
+    NzTypographyComponent,
+    NzRowDirective,
+    NzColDirective,
+    NzButtonComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
